e4c0c5f3c4fce5380bdbf7dffe89921829375242
Choeseonjeong <Choeseonjeong>
2023-08-26 01:05:11 +0900

    0826

---
+++ programmers_Lv.0/0825-10.py
@@ -0,0 +1,19 @@
+# n의 배수
+# 정수 num과 n이 매개 변수로 주어질 때,
+# num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록
+# solution 함수를 완성해주세요.
+
+
+def solution(num, n):
+    answer = 0
+    if (num % n) == 0:
+        answer += 1
+        return answer
+    else:
+        return answer
+
+
+# main
+
+num = int(input("num 입력"))
+n = int(input("n 입력"))
